72fd4d9cd135d7449df7ca5cd512a1cf04856bab
EnterMatch <y50001@hotmail.com>
2016-05-08 23:49:15 -0500

    Cliente y Persona terminados.

--- src/main/java/com/lab/restaurant/model/Cliente.java
+++ src/main/java/com/lab/restaurant/model/Cliente.java
@@ -1,5 +1,76 @@
 package main.java.com.lab.restaurant.model;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cliente extends Persona{
+
+	
+private static List<Cliente> lista;
+	
+	
+	static{
+		lista = new ArrayList<>();
+	}
+	
+	public Cliente(){}
+	
+	public Cliente(int id, String nombre) {
+		super(id, nombre);
+	}
+
+	public int create(Cliente reg){
+		int result = 0;
+		try{
+			lista.add(reg);
+			result = 1;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public List<Cliente> read(){
+		return lista;
+	}
+	
+	public Cliente read(int id){
+		Cliente reg = null;
+		for(Cliente x : read()){
+			if(x.getId() == id){
+				reg = x;
+			}
+		}
+		return reg;
+	}
+	
+	public int update(Cliente reg){
+		int result = 0;
+		try{
+			for(Cliente x : read()){
+				if(x.getId() == reg.getId()){
+					x.setNombre(reg.getNombre());
+				}
+			}
+			result = 1;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public int delete(int id){
+		int result = 0;
+		try{
+			for(Cliente x : read()){
+				if(x.getId() == id){
+					read().remove(x);
+				}
+			}
+			result = 1;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
--- src/main/java/com/lab/restaurant/model/Persona.java
+++ src/main/java/com/lab/restaurant/model/Persona.java
@@ -1,5 +1,29 @@
 package main.java.com.lab.restaurant.model;
 
-public abstract class Persona {
-
+class Persona {
+	
+	private int id;
+	private String nombre;
+	
+	public Persona(){};
+	
+	
+	public Persona(int id, String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
