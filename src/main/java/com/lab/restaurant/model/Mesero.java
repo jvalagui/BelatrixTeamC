6a6b286f281534235e21016b7048febb627e05b8
EnterMatch <y50001@hotmail.com>
2016-05-09 00:04:18 -0500

    Model Mesero terminado

--- src/main/java/com/lab/restaurant/model/Mesero.java
+++ src/main/java/com/lab/restaurant/model/Mesero.java
@@ -1,5 +1,86 @@
+
 package main.java.com.lab.restaurant.model;
 
-public class Mesero {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mesero extends Persona{
+	
+	private static List<Mesero> lista;
+	private boolean estado;
+	
+	public Mesero(){}
+
+	static{
+		lista = new ArrayList<>();
+	}
+	
+	public Mesero(int id, String nombre, boolean estado) {
+		super(id, nombre);
+		this.estado = estado;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	
+
+	public int create(Mesero reg){
+		int result = 0;
+		try{
+			lista.add(reg);
+			result = 1;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public List<Mesero> read(){
+		return lista;
+	}
+	
+	public Mesero read(int id){
+		Mesero reg = null;
+		for(Mesero x : read()){
+			if(x.getId() == id){
+				reg = x;
+			}
+		}
+		return reg;
+	}
+	
+	public int update(Mesero reg){
+		int result = 0;
+		try{
+			for(Mesero x : read()){
+				if(x.getId() == reg.getId()){
+					x.setNombre(reg.getNombre());
+				}
+			}
+			result = 1;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public int delete(int id){
+		int result = 0;
+		try{
+			for(Mesero x : read()){
+				if(x.getId() == id){
+					read().remove(x);
+				}
+			}
+			result = 1;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 }
